c536631a881a60aefd74e1764a2adcb7763c2795
takachan0012 <jj4068851@gmail.com>
2024-03-28 08:07:41 +0700

    add: skeleton shadcn ui

---
+++ src/components/ui/skeleton.jsx
@@ -0,0 +1,14 @@
+import { cn } from "@utils/cn"
+
+function Skeleton({
+  className,
+  ...props
+}) {
+  return (
+    (<div
+      className={cn("animate-pulse rounded-md bg-slate-100 dark:bg-slate-800", className)}
+      {...props} />)
+  );
+}
+
+export { Skeleton }
