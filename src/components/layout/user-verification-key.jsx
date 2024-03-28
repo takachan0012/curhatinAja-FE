7228c2adaa758e4f1af17fce0d15669b89dc2317
takachan0012 <jj4068851@gmail.com>
2024-03-28 12:32:59 +0700

    feat: user-verification layout

---
+++ src/components/layout/user-verification-key.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button } from "@components/ui/button";
+import { Input } from "@components/ui/input";
+import {
+    Dialog,
+    DialogHeader,
+    DialogTitle,
+    DialogContent
+} from "@components/ui/dialog";
+
+const VerificationKey = () => {
+    return (
+        <Dialog open='true'>
+            <DialogContent className='p-11 flex flex-col bg-[#DFFF67] gap-5'>
+                <DialogHeader className='flex flex-row justify-between lg:justify-normal'>
+                    <div className="lg:9/12">
+                        <DialogTitle className='text-2xl text-left'>Verifikasi private key</DialogTitle>
+                    </div>
+                </DialogHeader>
+                <Input placeholder='Private key...' className='border-2 border-black w-full' />
+                <Button className='font-bold w-full'>Verifikasi</Button>
+            </DialogContent>
+        </Dialog >
+    )
+}
+
+export { VerificationKey }
