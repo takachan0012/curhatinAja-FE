112147e0cf1ad1e46b6b76dd70b66a629f8d7605
takachan0012 <jj4068851@gmail.com>
2024-03-28 12:52:30 +0700

    feat: login layout

---
+++ src/components/layout/login.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Button } from "@components/ui/button";
+import { Input } from "@components/ui/input";
+import {
+    Dialog,
+    DialogHeader,
+    DialogTitle,
+    DialogDescription,
+    DialogContent,
+    DialogFooter
+} from "@components/ui/dialog";
+
+
+const Login = () => {
+    return (
+        <Dialog open='true'>
+            <DialogContent className='p-11 flex flex-col bg-[#DFFF67] gap-5'>
+                <DialogHeader className='flex flex-row justify-between lg:justify-normal'>
+                    <DialogTitle className='text-2xl text-left'>Masukan Private Key</DialogTitle>
+                </DialogHeader>
+                <Input placeholder='Private key...' className='border-2 border-black w-full' />
+                <Button className='font-bold w-full'>Login</Button>
+                <DialogFooter>
+                    <DialogDescription className='text-sm'>Belum punya akun? <a href="#" className='text-base decoration-solid underline hover:no-underline'>Register</a></DialogDescription>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog >
+    )
+}
+
+export { Login }
