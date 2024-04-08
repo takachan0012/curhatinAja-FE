1302524e38dc07ae695311de75898d00e1f99dd1
takachan0012 <jj4068851@gmail.com>
2024-04-08 15:52:21 +0700

    feat: new style

--- src/pages/initialPages.jsx
+++ src/pages/initialPages.jsx
@@ -1,60 +1,26 @@
 import React from "react";
 import { Button } from "@components/ui/button";
-import { Card, CardHeader, CardContent, CardTitle } from "@components/ui/card";
-import { Textarea } from "@components/ui/textarea";
-import { users } from "@utils/dataSample";
 import { authLogin } from "@utils/isLogin";
 import { Navigate } from "react-router-dom";
-import { Register } from "@components/layout/register";
-import { DialogTrigger } from "@components/ui/dialog";
+import IconSpeak from "@assets/user-speak-rounded-svgrepo-com.svg"
 
 const InitialPage = () => {
     const isLogin = authLogin();
     return (
         <>
             {isLogin ? <Navigate to="/feeds" /> :
-                <div className="flex flex-col gap-7 p-3 pb-7 bg-[#DFFF67] h-max lg:h-screen lg:justify-evenly lg:items-center lg:flex-row">
-                    <section>
-                        <header className="font-bold ">
-                            <h3 className="text-[2rem] lg:text-[50px]">Kenapa? </h3>
-                            <p className="font-black text-[3rem] lg:text-[128px]">
-                                <span>Sini </span>
-                                <span>CERITA</span>
-                            </p>
-                        </header>
-                        <div className="flex flex-col gap-2 lg:gap-4">
-                            <Textarea
-                                className='border-2 border-black h-[200px] text-base resize-none'
-                                placeholder='Tulis sesuatu...'
-                            />
-                            <Register>
-                                <DialogTrigger className='font-bold text-[25px] w-full -ms-2 outline-2 shadow-custom hover:shadow-[0_0px_0px_rgb(0,0,0)] bg-black ease-out hover:translate-x-2 hover:translate-y-1 transition-all rounded-md text-[#DFFF67] focus:shadow-[0_0px_0px_rgb(0,0,0)] focus:translate-x-2 focus:translate-y-1'>Post</DialogTrigger>
-                            </Register>
+                <div className="flex flex-col items-center">
+                    <header className="mt-11 w-11/12 bg-[#111020] min-h-[100px] p-4 drop-shadow-[35px_35px_35px_35px_rgba(234, 234, 234, 1)] flex items-center justify-between border-2 border-white">
+                        <div className="flex gap-2 items-center">
+                            <img src={IconSpeak} alt="icon-utarakan" className="h-[50px]" />
+                            <h1 className="header text-[#97FE99] text-[36px]">UTARAKAN</h1>
                         </div>
-                    </section>
-                    <Card className='p-4 -ms-2 bg-[#CAFEDB] h-[614px] lg:w-[435px] border-2 border-black shadow-custom hover:shadow-[0_0px_0px_rgb(0,0,0)] hover:translate-x-2 hover:translate-y-1 focus:translate-x-2 focus:translate-y-1'>
-                        <CardHeader className='flex items-center flex-row justify-between font-bold'>
-                            <span>Latest post</span>
-                            <Register>
-                                <DialogTrigger>
-                                    <span className="underline hover:no-underline">See more</span>
-                                </DialogTrigger>
-                            </Register>
-                        </CardHeader>
-                        <CardContent className='px-2 flex flex-col gap-2 overflow-hidden'>
-                            {users.map((user, index) => <Card key={index} className='h-[118px] border-2 border-black hover:cursor-pointer'>
-                                <CardHeader className='p-2 flex flex-row items-center'>
-                                    <CardTitle className='text-[1rem]'>
-                                        {user.username}
-                                        <span className='text-[1rem] text-slate-500 font-medium'> - 1h</span>
-                                    </CardTitle>
-                                </CardHeader>
-                                <CardContent>
-                                    <p className="line-clamp-3 text-[14px]">{user.post}</p>
-                                </CardContent>
-                            </Card>)}
-                        </CardContent>
-                    </Card>
+                        <div className="text-[24px] flex gap-2 items-center">
+                            <h1>Features</h1>
+                            <h1>Testimonials</h1>
+                        </div>
+                        <Button className="text-[24px]">Mulai Berbicara Sekarang!</Button>
+                    </header>
                 </div>
             }
         </>
